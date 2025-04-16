7ad1bd945b02c3e6583e7b23aeb7688565244b13
Suraj kr shaw <krsurajshaw17082004@gmail.com>
2025-04-16 23:25:42 +0530

    new bd

--- Backend/index.js
+++ Backend/index.js
@@ -24,10 +24,7 @@ app.use(cors({
 
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected locally...'))
   .catch((err) => console.error('❌ MongoDB connection failed:', err));
 
